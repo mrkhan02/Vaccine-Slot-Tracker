858b3bd06411d5fd32f3f253cb393b351f5b7e65
Akram <56334054+mrkhan02@users.noreply.github.com>
2021-11-10 13:59:50 +0530

    Update settings.py

--- covid/settings.py
+++ covid/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='extended211@gmail.com'
-EMAIL_HOST_PASSWORD='Extended@12@'
+EMAIL_HOST_USER='XXXXXXXXXXX@gmail.com'
+EMAIL_HOST_PASSWORD='**********'
 EMAIL_USE_TLS=True
 
 
